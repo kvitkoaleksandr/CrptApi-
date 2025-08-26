92e807977fc16c9b1cb6bf2c3959d0c8fdfe2f8c
kvitkoaleksandr <kvitok-993@mail.ru>
2025-08-26 21:19:54 +0300

    Implemented classes Document and Product

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public final class CrptApi {
 
     public interface TokenProvider {
@@ -17,6 +19,26 @@ public final class CrptApi {
         @Override
         public String toString() {
             return value;
+        }
+    }
+
+    public static class Document {
+        public String participant_inn;
+        public String producer_inn;
+        public String production_date;
+        public String production_type;
+        public List<Product> products;
+
+        public static class Product {
+            public String certificate_document;
+            public String certificate_document_date;
+            public String certificate_document_number;
+            public String owner_inn;
+            public String producer_inn;
+            public String production_date;
+            public String tnved_code;
+            public String uit_code;
+            public String uitu_code;
         }
     }
 }
