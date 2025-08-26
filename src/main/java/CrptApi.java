58ec47ed863b24ac294599e8d44365588e3a2da2
kvitkoaleksandr <kvitok-993@mail.ru>
2025-08-26 20:56:07 +0300

    1 commit Done CrptApi

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -1,3 +1,22 @@
 public final class CrptApi {
 
+    public interface TokenProvider {
+        String getToken();
+    }
+
+    public static final class DocumentId {
+        public final String value;
+
+        public DocumentId(String value) {
+            if (value == null) {
+                throw new NullPointerException("value is null");
+            }
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
 }
